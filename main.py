d4bef0c69a4a3b32ce394f3f398ba5b732b56c2f
Riccardo Trivellato <6997999+rtrive@users.noreply.github.com>
2022-05-19 15:19:35 +0100

    second commit

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Firefox()
-driver.get("https://account.formula1.com/#/en/login?lead_source=web_fantasy&redirect=https%3A%2F%2Ffantasy.formula1.com%2Fapp%2F%23%2F")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
+
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36')
+driver = webdriver.Chrome(options=options)
+driver.get("https://account.formula1.com/#/en/login")
+cookie_accept = driver.find_element_by_id("truste-consent-button").click()
+elem = driver.find_element_by_name("Login")
+elem = driver.find_element(by=By.NAME, value="Login")
 elem.clear()
-elem.send_keys("pycon")
+elem.send_keys("")
+elem = driver.find_element(by=By.NAME, value="Password")
+elem.clear()
+elem.send_keys("")
 elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+sleep(10)
+driver.get_screenshot_as_file("./test.png")
 driver.close()
