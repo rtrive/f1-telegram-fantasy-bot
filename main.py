619cd3d76e210cb0430c700ff740fab88580a55e
Marco Comi <marco.comi@aol.com>
2022-05-19 19:09:23 +0200

    Perform login operation

--- main.py
+++ main.py
@@ -2,22 +2,14 @@ import os
 import sys
 from time import sleep
 from dotenv import load_dotenv
-
-from selenium import webdriver
-
+import undetected_chromedriver as uc
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
 def create_diver():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--disable-blink-features=AutomationControlled')
-    options.add_argument("--disable-extensions")
-    options.add_experimental_option('useAutomationExtension', False)
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(options=options)
-    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+    options = uc.ChromeOptions()
+    driver = uc.Chrome(options=options)
     return driver
 
 
@@ -45,7 +37,7 @@ if __name__ == "__main__":
     if not password:
         sys.exit("Missing password")
     driver = create_diver()
-    go_to_page(driver, "https://account.formula1.com/#/en/login")
+    go_to_page(driver, "https://account.formula1.com/#/en/login?lead_source=web_fantasy&redirect=https%3A%2F%2Ffantasy.formula1.com%2Fapp%2F%23%2F")
     click_button_by_id(driver, By.ID, "truste-consent-button")
 
     fill_text_area(driver, By.NAME, "Login", username)
