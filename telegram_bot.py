fc225b72d13eced4419008ec29184d3418155e6a
Riccardo Trivellato <6997999+rtrive@users.noreply.github.com>
2022-05-30 11:33:33 +0200

    feat: start and echo command

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,33 @@
-from telegram.ext import Application
+from telegram import Update
+from telegram.ext import (
+    filters,
+    ApplicationBuilder,
+    CallbackContext,
+    CommandHandler,
+    MessageHandler,
+)
+
+
+async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a F1 Fantasy bot, please talk to me!"
+    )
+
+
+async def echo(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=update.message.text
+    )
 
 
 class Bot:
-
     def __init__(self, api_key: str):
-        self.application = Application.builder().token(api_key).build()
+        try:
+            self.application = ApplicationBuilder().token(api_key).build()
+            start_handler = CommandHandler("start", start)
+            echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+            self.application.add_handler(start_handler)
+            self.application.add_handler(echo_handler)
+            self.application.run_polling()
+        except Exception as e:
+            print(e)
