f39a6b62333bf1827c0995665023e772db2682c1
Riccardo Trivellato <6997999+rtrive@users.noreply.github.com>
2022-05-30 12:11:43 +0200

    feat: move commands inside class. Api key optional

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from telegram import Update
 from telegram.ext import (
     filters,
@@ -8,26 +9,25 @@ from telegram.ext import (
 )
 
 
-async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="I'm a F1 Fantasy bot, please talk to me!"
-    )
-
-
-async def echo(update: Update, context: CallbackContext.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id, text=update.message.text
-    )
-
-
 class Bot:
-    def __init__(self, api_key: str):
+    def __init__(self, api_key: Optional[str]):
         try:
             self.application = ApplicationBuilder().token(api_key).build()
-            start_handler = CommandHandler("start", start)
-            echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+            start_handler = CommandHandler("start", self.start)
+            echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), self.echo)
             self.application.add_handler(start_handler)
             self.application.add_handler(echo_handler)
             self.application.run_polling()
         except Exception as e:
             print(e)
+
+    async def start(self, update: Update, context: CallbackContext.DEFAULT_TYPE):
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="I'm a F1 Fantasy bot, please talk to me!",
+        )
+
+    async def echo(self, update: Update, context: CallbackContext.DEFAULT_TYPE):
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id, text=update.message.text
+        )
