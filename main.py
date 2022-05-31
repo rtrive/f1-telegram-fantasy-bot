01bd351b8d2121f1b1ce75abb897f443624e0a6c
Riccardo Trivellato <6997999+rtrive@users.noreply.github.com>
2022-05-31 18:19:49 +0200

    Telegram bot (#4)
    * feat: telegram bot
    
    * feat: api key telegram bot
    
    * feat: init bot
    
    * Rename file
    
    * Update gitignore
    
    * Pass API key to Bot constructor
    
    * feat: start and echo command
    
    * feat: init bot
    
    * feat: move commands inside class. Api key optional
    
    * feat: pass cookies to bot
    
    * feat: const url f1
    
    * chore: pass url to bot
    
    * feat: add selenium wire
    
    * wip: standings
    
    * fix: restore keys
    
    * wip: cookies
    
    * chore: parsing url encode
    
    * chore: remove print header
    
    * fix: wrong parameter
    
    * chore: refactor cookie
    
    * Update main.py
    
    Co-authored-by: Marco Comi <marco.comi@aol.com>
    
    * Update main.py
    
    Co-authored-by: Marco Comi <marco.comi@aol.com>
    
    * Update main.py
    
    Co-authored-by: Marco Comi <marco.comi@aol.com>
    
    Co-authored-by: Marco Comi <marco.comi@aol.com>

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 import os
 import sys
+import urllib.parse
+from time import sleep
 from typing import Any, List
-from undetected_chromedriver import Chrome as uc_chrome  # type: ignore
+from seleniumwire.undetected_chromedriver import Chrome as uc_chrome  # type: ignore
 from undetected_chromedriver import ChromeOptions as uc_chrome_options  # type: ignore
 from dotenv import load_dotenv
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from telegram_bot import Bot
 
 
-def manipulate_cookies(cookies: List[dict]) -> dict:
-    new_cookies = {}
+F1_FANTASY_LOGIN_URL = "https://account.formula1.com/#/en/login?lead_source=web_fantasy&redirect=https%3A%2F%2Ffantasy.formula1.com%2Fapp%2F%23%2F"  # noqa: E501
+
+
+def get_player_cookie(driver: uc_chrome) -> str:
+
+    for resp in driver.requests:
+        if resp.url == "https://fantasy-api.formula1.com/f1/2022/sessions?v=1":
+            player_cookie = resp.response.headers.get("Set-Cookie").split(";")[0]
+
+    return player_cookie
+
+
+def manipulate_cookies(cookies: List[dict]) -> str:
+    new_cookies = ""
     for cookie in cookies:
-        new_cookies[cookie["name"]] = cookie["value"]
+        if (
+            cookie["name"] == "login"
+            or cookie["name"] == "login-session"
+            or cookie["name"] == "user-metadata"
+        ):
+            new_cookies += f'{cookie["name"]}={urllib.parse.quote(cookie["value"])}; '
+        else:
+            new_cookies += f'{cookie["name"]}={cookie["value"]}; '
     return new_cookies
 
 
 def create_diver() -> uc_chrome:
     options = uc_chrome_options()
+    options.add_argument("--headless")
     driver = uc_chrome(options=options)
     return driver
 
@@ -47,7 +70,7 @@ if __name__ == "__main__":
     driver = create_diver()
     go_to_page(
         driver,
-        "https://account.formula1.com/#/en/login?lead_source=web_fantasy&redirect=https%3A%2F%2Ffantasy.formula1.com%2Fapp%2F%23%2F",  # noqa: E501
+        F1_FANTASY_LOGIN_URL,
     )
     click_button_by_id(driver, By.ID, "truste-consent-button")
 
@@ -56,5 +79,11 @@ if __name__ == "__main__":
     elem = fill_text_area(driver, By.NAME, "Password", password)
     elem.send_keys(Keys.RETURN)
     cookies = driver.get_cookies()
-    cookies = manipulate_cookies(cookies)
+    sleep(30)
     driver.close()
+    cookies = get_player_cookie(driver)
+    print("Ready to be used with bot")
+    telegram_bot_api_key = os.getenv("TELEGRAM_BOT_API_KEY")
+    if not telegram_bot_api_key:
+        sys.exit("Missing telegram api key")
+    Bot(telegram_bot_api_key, cookies, F1_FANTASY_LOGIN_URL)
