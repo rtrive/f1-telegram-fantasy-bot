b2a150225dc1e826095bc6b4a0ae49a412c0a6ce
Marco Comi <marco.comi@aol.com>
2022-06-06 17:24:53 +0200

    Cleanup code and move handlers into main

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from time import sleep
 from typing import List
 from seleniumwire.undetected_chromedriver import Chrome as uc_chrome  # type: ignore
 from dotenv import load_dotenv
+from telegram.ext import CommandHandler, MessageHandler, filters
+
 from telegram_bot import Bot
 from credentials import Credentials
 from uc_driver import ChromeDriver
-
 
 F1_FANTASY_LOGIN_URL = "https://account.formula1.com/#/en/login?lead_source=web_fantasy&redirect=https%3A%2F%2Ffantasy.formula1.com%2Fapp%2F%23%2F"  # noqa: E501
 
@@ -20,20 +21,6 @@ def get_player_cookie(driver: uc_chrome) -> str:
             player_cookie = resp.response.headers.get("Set-Cookie").split(";")[0]
 
     return player_cookie
-
-
-def manipulate_cookies(cookies: List[dict]) -> str:
-    new_cookies = ""
-    for cookie in cookies:
-        if (
-            cookie["name"] == "login"
-            or cookie["name"] == "login-session"
-            or cookie["name"] == "user-metadata"
-        ):
-            new_cookies += f'{cookie["name"]}={urllib.parse.quote(cookie["value"])}; '
-        else:
-            new_cookies += f'{cookie["name"]}={cookie["value"]}; '
-    return new_cookies
 
 
 if __name__ == "__main__":
@@ -48,13 +35,18 @@ if __name__ == "__main__":
         credentials=credentials,
     )
 
-    cookies = driver.get_cookies()
-    cookies = manipulate_cookies(cookies=cookies)
     sleep(20)
     driver.close()
     cookies = get_player_cookie(driver)
-    print("Ready to be used with bot")
     telegram_bot_api_key = os.getenv("TELEGRAM_BOT_API_KEY")
     if not telegram_bot_api_key:
         sys.exit("Missing telegram api key")
-    Bot(telegram_bot_api_key, cookies, F1_FANTASY_LOGIN_URL)
+    fantasy_bot = Bot(api_key=telegram_bot_api_key)
+
+    print("Registering handlers")
+    fantasy_bot.application.add_handler(CommandHandler("start", Bot.start_bot_handler()))
+    fantasy_bot.application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), Bot.echo_handler()))
+    fantasy_bot.application.add_handler(CommandHandler("standings", Bot.get_standings(cookies)))
+
+    print("Starting bot")
+    fantasy_bot.start_bot()
--- telegram_bot.py
+++ telegram_bot.py
@@ -2,43 +2,47 @@ import requests  # type: ignore
 from typing import Optional
 from telegram import Update
 from telegram.ext import (
-    filters,
     ApplicationBuilder,
     CallbackContext,
-    CommandHandler,
-    MessageHandler,
 )
 
 
 class Bot:
-    def __init__(self, api_key: Optional[str], cookies: str, url: str):
+    def __init__(self, api_key: Optional[str]):
         try:
-            self.cookies = cookies
-            self.url = url
             self.application = ApplicationBuilder().token(api_key).build()
-            start_handler = CommandHandler("start", self.start)
-            echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), self.echo)
-            self.application.add_handler(CommandHandler("standings", self.standings))
-            self.application.add_handler(start_handler)
-            self.application.add_handler(echo_handler)
+        except Exception as e:
+            print(e)
+
+    def start_bot(self):
+        try:
             self.application.run_polling()
         except Exception as e:
             print(e)
 
-    async def start(self, update: Update, context: CallbackContext.DEFAULT_TYPE):
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="I'm a F1 Fantasy bot, please talk to me!",
-        )
+    @staticmethod
+    def start_bot_handler():
+        async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="I'm a F1 Fantasy bot, please talk to me!",
+            )
+        return start
 
-    async def echo(self, update: Update, context: CallbackContext.DEFAULT_TYPE):
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=update.message.text
-        )
+    @staticmethod
+    def echo_handler():
+        async def echo(update: Update, context: CallbackContext.DEFAULT_TYPE):
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id, text=update.message.text
+            )
+        return echo
 
-    async def standings(self, update: Update, context: CallbackContext.DEFAULT_TYPE):
-        req = requests.get(
-            url="https://fantasy-api.formula1.com/f1/2022/league_entrants?v=1",
-            headers={"Cookie": self.cookies},
-        )
-        print(req.json())
+    @staticmethod
+    def get_standings(cookies: str):
+        async def standings(update: Update, context: CallbackContext.DEFAULT_TYPE):
+            req = requests.get(
+                url="https://fantasy-api.formula1.com/f1/2022/league_entrants?v=1",
+                headers={"Cookie": cookies},
+            )
+            print(req.json())
+        return standings
