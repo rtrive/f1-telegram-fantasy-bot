76aceb1611f2c636423dde4fa30134c20afa6652
Marco Comi <marco.comi@aol.com>
2022-06-07 10:44:55 +0200

    Refactor bot (#8)
    * Update dependencies
    
    * Cleanup code and move handlers into main
    
    * Install prettytable
    
    * Add F1_FANTASY_LEAGUE_ID
    
    * Small refactor and first draft of get f1 fantasy standings
    
    * Move out method that parse HTTP response
    
    This is a work in progress
    
    * chore: lint
    
    Co-authored-by: Riccardo Trivellato <6997999+rtrive@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import os
 import sys
-import urllib.parse
 from time import sleep
-from typing import List
 from seleniumwire.undetected_chromedriver import Chrome as uc_chrome  # type: ignore
+from seleniumwire.undetected_chromedriver import (
+    ChromeOptions as uc_chrome_options,
+)
 from dotenv import load_dotenv
+from telegram.ext import CommandHandler, MessageHandler, filters
+
 from telegram_bot import Bot
 from credentials import Credentials
 from uc_driver import ChromeDriver
-
 
 F1_FANTASY_LOGIN_URL = "https://account.formula1.com/#/en/login?lead_source=web_fantasy&redirect=https%3A%2F%2Ffantasy.formula1.com%2Fapp%2F%23%2F"  # noqa: E501
 
@@ -22,24 +24,12 @@ def get_player_cookie(driver: uc_chrome) -> str:
     return player_cookie
 
 
-def manipulate_cookies(cookies: List[dict]) -> str:
-    new_cookies = ""
-    for cookie in cookies:
-        if (
-            cookie["name"] == "login"
-            or cookie["name"] == "login-session"
-            or cookie["name"] == "user-metadata"
-        ):
-            new_cookies += f'{cookie["name"]}={urllib.parse.quote(cookie["value"])}; '
-        else:
-            new_cookies += f'{cookie["name"]}={cookie["value"]}; '
-    return new_cookies
-
-
 if __name__ == "__main__":
     load_dotenv()
 
-    driver = ChromeDriver()
+    chrome_options = uc_chrome_options()
+    # chrome_options.add_argument("--headless")
+    driver = ChromeDriver(options=chrome_options)
     credentials = Credentials(
         username=os.getenv("USERNAME"), password=os.getenv("PASSWORD")
     )
@@ -48,13 +38,29 @@ if __name__ == "__main__":
         credentials=credentials,
     )
 
-    cookies = driver.get_cookies()
-    cookies = manipulate_cookies(cookies=cookies)
-    sleep(20)
-    driver.close()
+    sleep(30)
+    # driver.close()
     cookies = get_player_cookie(driver)
-    print("Ready to be used with bot")
     telegram_bot_api_key = os.getenv("TELEGRAM_BOT_API_KEY")
     if not telegram_bot_api_key:
         sys.exit("Missing telegram api key")
-    Bot(telegram_bot_api_key, cookies, F1_FANTASY_LOGIN_URL)
+    fantasy_bot = Bot(api_key=telegram_bot_api_key)
+    league_id = os.getenv("F1_FANTASY_LEAGUE_ID")
+    if not league_id:
+        sys.exit("Missing league id")
+
+    print("Registering handlers")
+    fantasy_bot.application.add_handler(
+        CommandHandler("start", Bot.start_bot_handler())
+    )
+    fantasy_bot.application.add_handler(
+        MessageHandler(filters.TEXT & (~filters.COMMAND), Bot.echo_handler())
+    )
+    fantasy_bot.application.add_handler(
+        CommandHandler(
+            "standings", Bot.get_standings(cookies=cookies, league_id=league_id)
+        )
+    )
+
+    print("Starting bot")
+    fantasy_bot.start_bot()
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,44 +1,80 @@
 import requests  # type: ignore
-from typing import Optional
+from typing import Optional, Union
+
+from requests import Response
 from telegram import Update
+from telegram.constants import ParseMode
 from telegram.ext import (
-    filters,
     ApplicationBuilder,
     CallbackContext,
-    CommandHandler,
-    MessageHandler,
 )
+import prettytable as pt
 
 
 class Bot:
-    def __init__(self, api_key: Optional[str], cookies: str, url: str):
+    def __init__(self, api_key: Optional[str]):
         try:
-            self.cookies = cookies
-            self.url = url
             self.application = ApplicationBuilder().token(api_key).build()
-            start_handler = CommandHandler("start", self.start)
-            echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), self.echo)
-            self.application.add_handler(CommandHandler("standings", self.standings))
-            self.application.add_handler(start_handler)
-            self.application.add_handler(echo_handler)
+        except Exception as e:
+            print(e)
+
+    def start_bot(self):
+        try:
             self.application.run_polling()
         except Exception as e:
             print(e)
 
-    async def start(self, update: Update, context: CallbackContext.DEFAULT_TYPE):
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="I'm a F1 Fantasy bot, please talk to me!",
-        )
+    @staticmethod
+    def start_bot_handler():
+        async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="I'm a F1 Fantasy bot, please talk to me!",
+            )
 
-    async def echo(self, update: Update, context: CallbackContext.DEFAULT_TYPE):
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=update.message.text
-        )
+        return start
 
-    async def standings(self, update: Update, context: CallbackContext.DEFAULT_TYPE):
-        req = requests.get(
-            url="https://fantasy-api.formula1.com/f1/2022/league_entrants?v=1",
-            headers={"Cookie": self.cookies},
-        )
-        print(req.json())
+    @staticmethod
+    def echo_handler():
+        async def echo(update: Update, context: CallbackContext.DEFAULT_TYPE):
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id, text=update.message.text
+            )
+
+        return echo
+
+    @staticmethod
+    def get_standings(cookies: str, league_id: str):
+        league_url = f"https://fantasy-api.formula1.com/f1/2022/leaderboards/leagues?v=1&league_id={league_id}"  # noqa: E501
+
+        async def get_f1_fantasy_standings(
+            update: Update, context: CallbackContext.DEFAULT_TYPE
+        ):
+            req = requests.get(
+                url=league_url,
+                headers={"Cookie": cookies},
+            )
+            standing_message = get_standings_as_string(req)
+            # FIXME: At the moment the parse_mode has been set to MARKDOWN.
+            # We have to decide how to show the standings
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=f"```{standing_message}```",
+                parse_mode=ParseMode.MARKDOWN_V2,
+            )
+
+        return get_f1_fantasy_standings
+
+
+# TODO Define the format of the output message
+def get_standings_as_string(req: Union[Response, Response]) -> str:
+    status_code = req.status_code
+    if status_code == 200:
+        leaderboard = req.json()["leaderboard"]["leaderboard_entrants"]
+
+        table = pt.PrettyTable(["Username", "Points"])
+        for entry in leaderboard:
+            table.add_row([entry["username"], entry["score"]])
+        return f"```{table}```"
+    else:
+        return f"Ko with status code {status_code}"
