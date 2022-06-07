3b6a6af898ad6c70c3b99eee38408e00f3c10453
Riccardo Trivellato <6997999+rtrive@users.noreply.github.com>
2022-06-07 11:38:44 +0200

    chore: add headless

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     load_dotenv()
 
     chrome_options = uc_chrome_options()
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     seleniumwire_options = {"connection-keep-alive": True, "disable-encoding": True}
     driver = ChromeDriver(
         options=chrome_options, seleniumwire_options=seleniumwire_options
