4a8184d5177feebc273b9153c2bd660841bf8993
Riccardo Trivellato <6997999+rtrive@users.noreply.github.com>
2022-06-21 11:59:33 +0200

    feat: add name parameter. Prettify table

--- src/adapters/picked_player_adapters.py
+++ src/adapters/picked_player_adapters.py
@@ -1,7 +1,24 @@
 from typing import List
-from core.picked_player import PickedPlayer
 
 import prettytable as pt
+import requests  # type: ignore
+from core.picked_player import PickedPlayer
+
+f1_all_players = {}
+
+
+def player_id_to_name() -> dict:
+    global f1_all_players
+    if not f1_all_players:
+        f1_players_req = requests.get(
+            url="https://fantasy-api.formula1.com/f1/2022/players"
+        )
+        f1_players = (f1_players_req.json())["players"]
+        f1_all_players = {}
+        for f1_player in f1_players:
+            f1_all_players[f1_player["id"]] = f1_player["last_name"]
+        return f1_all_players
+    return f1_all_players
 
 
 def to_picked_players(json: dict) -> List[PickedPlayer]:
@@ -12,11 +29,14 @@ def to_picked_players(json: dict) -> List[PickedPlayer]:
 
 
 def to_picked_player(json: dict) -> PickedPlayer:
+    players = player_id_to_name()
     return PickedPlayer(
         player_id=json["player_id"],
+        player_name=players[json["player_id"]],
         team_name=json["team_name"],
         team_abbreviation=json["team_abbreviation"],
         position=json["position"],
+        position_abbreviation=json["position_abbreviation"],
         score=json["score"],
     )
 
@@ -24,7 +44,13 @@ def to_picked_player(json: dict) -> PickedPlayer:
 def picker_players_to_pretty_table(
     picker_players: List[PickedPlayer],
 ) -> pt.PrettyTable:
-    table = pt.PrettyTable(["Player ID", "Type", "Team", "Score"])
+    table = pt.PrettyTable(["Name", "Team", "Score"])
     for e in picker_players:
-        table.add_row([e.player_id, e.position, e.team_abbreviation, e.score])
+        table.add_row(
+            [
+                f"{e.player_name}({e.position_abbreviation})",
+                e.team_abbreviation,
+                e.score,
+            ]
+        )
     return table
