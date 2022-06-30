e0faacfc75a70fe994200f7cc46b856034eb6fff
Marco Comi <marco.comi@aol.com>
2022-06-30 09:33:26 +0200

    Add handler to set reminder for lineup

--- src/bot/telegram_command.py
+++ src/bot/telegram_command.py
@@ -3,6 +3,7 @@ TELEGRAM_START_COMMAND = "start"
 TELEGRAM_FANTASY_LAST_GP_STANDING_COMMAND = "last_gp_standing"
 TELEGRAM_FANTASY_STANDING_COMMAND = "standing"
 TELEGRAM_FANTASY_TEAM_COMMAND = "last_gp_team_result"
+TELEGRAM_FANTASY_LINEUP_REMINDER = "lineup_reminder"
 
 
 class TelegramCommand:
@@ -31,5 +32,15 @@ COMMANDS = [
     TelegramCommand(
         name=TELEGRAM_FANTASY_TEAM_COMMAND,
         description="Get F1 Fantasy league standing for single team",
+    ),
+    TelegramCommand(
+        name=TELEGRAM_FANTASY_LINEUP_REMINDER,
+        description=f"Remind me to make the lineup. You can set the minutes "
+        f"before the starting date to be reminded."
+        f"\n/{TELEGRAM_FANTASY_LINEUP_REMINDER} <minutes>\n"
+        f"Example: /{TELEGRAM_FANTASY_LINEUP_REMINDER} 10\n"
+        f"Example: /{TELEGRAM_FANTASY_LINEUP_REMINDER} 10 30\n"
+        f"If no parameters are provided, the default value is 30 minutes.\n"
+        f"If an invalid value is provided, it will be ignored.",
     ),
 ]
