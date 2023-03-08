a5643857a8fd77efb7e9ddfad0bb8c91317c2692
Riccardo Trivellato <6997999+rtrive@users.noreply.github.com>
2023-03-08 18:10:15 +0100

    Get last race standing (#50)
    * fix: wrong status and change data format
    
    * feat: last race standing
    
    * Users (#46)
    
    * chore: change standings url
    
    * feat: new users
    
    * chore: api finded
    
    * chore: remove external and globalid
    
    * fix: disable web security for cors (#49)
    
    * fix: typo
    
    * chore: remove useless parameter
    
    * fix: remove useless parameter

--- src/adapters/season_adapters.py
+++ src/adapters/season_adapters.py
@@ -6,7 +6,7 @@ from core.race import Race, RaceStatus
 
 # TODO: Refactor using match case (it looks like ufmt doesn't work with that)
 def to_race_status(race_state: str) -> RaceStatus:
-    if race_state == "1":
+    if race_state == "4":
         return RaceStatus.COMPLETED
     elif race_state == "0":
         return RaceStatus.SCHEDULED
@@ -16,11 +16,13 @@ def to_race_status(race_state: str) -> RaceStatus:
 
 def to_race(game_period: dict) -> Race:
     return Race(
-        race_id=game_period["RaceId"],
+        race_id=game_period["MeetingNumber"],  # To be checked with second race
         name=game_period["MeetingName"],
         starts_at=datetime.strptime(
-            game_period["SessionStartDate"], "%Y-%m-%dT%H:%M:%S.%fZ"
-        ),
+            game_period["SessionStartDateISO8601"], "%Y-%m-%dT%H:%M:%S%z"
+        ).replace(
+            tzinfo=None
+        ),  # Check if timezone will be a problem
         status=to_race_status(game_period["MatchStatus"]),
     )
 
--- src/core/user.py
+++ src/core/user.py
@@ -1,4 +1,4 @@
 class User:
-    def __init__(self, username: str, user_id: str, global_id: str, external_id: str):
+    def __init__(self, username: str, user_id: str):
         self.username = username
         self.user_id = user_id
